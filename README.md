84adde51023f8fc1eff1760bcdee5120b69216ca
James Clarke <jamesmorganclarke@hotmail.co.uk>
2023-10-31 23:09:10 +0000

    Deleted files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repo downloads, unpacks, and anaylises web extensions looking at their security and privacy.
 
 ## 1. Get a list of extenions
-First we need to get a list of extenions. This is done using the console in a web browser, by finding the relevent extensions on the Google Store. We then get the URL of each extension by running the scrip "get_extension_urls.js" in the developer console and saving the results to a txt file.
+First we need to get a list of extenions. This is done using the console in a web browser, by finding the relevent extensions on the Google Store. We then get the URL of each extension by running the scrip "get_extension_urls.js" in the developer console and saving the results to a txt file. Need to set profile which is already logged in, for some extensions which include "mature content".
 
 ## 2. Downloading extensions
 Next, we need to download the crx files for each extension. This can be done using the script "download_extensions.py", and supplying it with the relevent file from above. While doing this, the script will also gather data about each extension, such as name, developer, rating, number of downloads, etc.
--- dowload_extensions.py
+++ dowload_extensions.py
@@ -1,23 +1,46 @@
 """
 Downloads extensions and saves info about them based on a txt file containing urls.
 """
-
-import time
-import os
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
-import urllib.request
-import re
+import urllib.request,re, argparse, time, os, sys
+from tqdm import tqdm
+
+#Function to try create dir if it doesn't exist
+def create_directory(directory_path):
+    Path(directory_path).mkdir(parents=True, exist_ok=True)
+
+current_dir=os.getcwd()
+
+#Create dir for downloads
+
+create_directory(os.path.join(current_dir,"crx_files"))
+
+#Take input of txt file
+parser = argparse.ArgumentParser("Download extenions")
+parser.add_argument("urls", help="Input the name of the txt containing the urls.", type=str)
+args = parser.parse_args()
+urls_name = args.urls
 
 
-rootdir=os.getcwd()
-url_dir = os.path.join(rootdir+"extension_urls")
+#Check if csv file exists
+url_file = os.path.join(current_dir,"extension_urls",urls_name+".txt")
+if(os.path.isfile(url_file)):
+    print("File %s exists, starting to download" %(urls_name))
+else:
+    print("File %s doesn't exists, stopping" %(url_file))
+    sys.exit()
 
+#Create download folder
+download_folder = os.path.join(current_dir,"crx_files",urls_name)
+create_directory(download_folder)
 
-extension_list = open(os.path.join(url_dir,'screen_reader.txt'), 'r', encoding='utf-8') #,encoding="utf8"
+extension_list = open(url_file, 'r', encoding='utf-8') #,encoding="utf8"
 extension_urls = extension_list.readlines()
+
 # you may also want to remove whitespace characters like `\n` at the end of each line
 extension_urls = [x.strip() for x in extension_urls]
 
@@ -26,30 +49,34 @@ options = FirefoxOptions()
 options.set_preference("browser.download.folderList", 2)
 options.set_preference("browser.download.manager.showWhenStarting", False)
 options.set_preference("browser.download.dir", 'screen_reader')
+#Need to set profile which is already logged in, for some extensions which include "mature content"
+options.add_argument("-profile")
+options.add_argument("/Users/jc02788/Library/Application Support/Firefox/Profiles/yqtf8iie.default-release")
 #profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-chrome-extension")
 
 driver = webdriver.Firefox(options=options)
 
-open('ExtensionInformation.csv', 'w').close()
+extension_file = urls_name+".csv"
 
-folder = 'screen_reader_crx_files/'
-    
-for ext in extension_urls:  
-	# This section gets extension information  
-    print(ext)
-    try:
+open(extension_file, 'w').close()
+with tqdm(total=len(extension_urls)) as pbar:
+    for ext in extension_urls:  
+        # This section gets extension information 
+        #try:
         driver.get(ext)
-        list_file = open('ExtensionInformation.csv', 'a', encoding='utf-8') #
+        list_file = open(extension_file, 'a', encoding='utf-8') #
 
         time.sleep(3) # Let the user actually see something!
-        try:
-            extension_name = driver.find_element(by=By.CLASS_NAME, value='e-f-w').text
-        except:
-            extension_name = "Not Mentioned"
-            #Make it so it errors out if it is not mentioned
-            errors_file = open('Error URL.txt', 'a', encoding='utf-8')
-            errors_file.write(ext+' -Getting name\n')
-            errors_file.close() 
+        #try:
+        extension_name = driver.find_element(by=By.CLASS_NAME, value='e-f-w').text
+        print(extension_name)
+        
+        """except:
+        extension_name = "Not Mentioned"
+        #Make it so it errors out if it is not mentioned
+        errors_file = open('Error URL.txt', 'a', encoding='utf-8')
+        errors_file.write(ext+' -Getting name\n')
+        errors_file.close()""" 
 
         try:
             extension_producer =  driver.find_element(by=By.CLASS_NAME, value='e-f-bb-K').find_element(by=By.CLASS_NAME, value='e-f-y').text
@@ -87,15 +114,15 @@ for ext in extension_urls:
         try:
             extension = '.crx'
             #Checks if the file already exists, if it does creates a versions with number at the end
-            if os.path.isfile(folder+re.sub('[<>:"/\|?*,]',' ',extension_name)+".crx"):
+            if os.path.isfile(os.path.join(download_folder,re.sub('[<>:"/\|?*,]',' ',extension_name))+".crx"):
                 i = 1
-                while os.path.isfile(folder+re.sub('[<>:"/\|?*,]',' ',extension_name)+'_'+str(i)+".crx"):
+                while os.path.isfile(os.path.join(download_folder,re.sub('[<>:"/\|?*,]',' ',extension_name))+'_'+str(i)+".crx"):
                     i += 1
                 
-                urllib.request.urlretrieve(url, folder+re.sub('[<>:"/\|?*,]',' ',extension_name)+'_'+str(i)+".crx")
+                urllib.request.urlretrieve(url, os.path.join(download_folder,re.sub('[<>:"/\|?*,]',' ',extension_name))+'_'+str(i)+".crx")
                 file_name  = re.sub('[<>:"/\|?*,]',' ',extension_name)+'_'+str(i)+".crx"
             else:
-                urllib.request.urlretrieve(url, folder+re.sub('[<>:"/\|?*,]',' ',extension_name)+".crx")
+                urllib.request.urlretrieve(url, os.path.join(download_folder,re.sub('[<>:"/\|?*,]',' ',extension_name))+".crx")
                 file_name = re.sub('[<>:"/\|?*,]',' ',extension_name)+".crx"
         except:
             errors_file = open('Error URL.txt', 'a', encoding='utf-8')
@@ -105,18 +132,13 @@ for ext in extension_urls:
 
         list_file.write(re.sub('[<>:"/\|?*,]',' ',extension_name) + ',' + ext + ',' + extension_producer + ',' + extension_category + ',' + extension_population + ',' + extension_ratings + ',' + extension_no_people_rated + ',' + file_name+ '\n')
         list_file.close()
-    except:
-        errors_file = open('Error URL.txt', 'a', encoding='utf-8')
-        errors_file.write(ext+'\n')
-        errors_file.close()
+        #except:
+        #    errors_file = open('Error URL.txt', 'a', encoding='utf-8')
+        #    errors_file.write(ext+'\n')
+        #    errors_file.close()
 
-    time.sleep(2)
+        time.sleep(2)
+        pbar.update(1)
 
+#TODO: Add checks that all extensions have been downloaded
 driver.quit()  
-    
-
-
-
-
-
-# https://clients2.google.com/service/update2/crx?response=redirect&prodversion=57.0&x=id%3Dnckgahadagoaajjgafhacjanaoiihapd%26installsource%3Dondemand%26uc
