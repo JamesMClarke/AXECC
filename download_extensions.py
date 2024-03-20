b2db4aa2160f0d2ce3664d8d4333717856ecfbef
James Clarke <jamesmorganclarke@hotmail.co.uk>
2024-03-20 21:53:10 +0000

    Updated downloading extensions

---
+++ download_extensions.py
@@ -0,0 +1,128 @@
+from common import *
+from tqdm import tqdm
+import urllib.request, argparse, time, os, re
+from bs4 import BeautifulSoup
+
+
+current_dir = os.getcwd()
+crx_folder = os.path.join(current_dir,"crx_files")
+
+#Create dir for all crx files
+create_directory(crx_folder)
+
+#Take input of txt file
+parser = argparse.ArgumentParser("Download extenions")
+parser.add_argument("urls", help="File contating the urls of to be downloaded", type=str)
+parser.add_argument("-r",'--resume', dest="resume", help="Resume donwloading extensions", action='store_true')
+parser.add_argument("-v",'--verbose', action='store_true')
+args = parser.parse_args()
+urls = args.urls
+resume = args.resume
+verbose = args.verbose
+
+#Get file name
+urls_name = os.path.basename(urls).split(".")[0]
+
+#Check if csv file exists
+url_file = os.path.join(current_dir,urls)
+if(not os.path.isfile(url_file)):
+    print("File %s doesn't exists, stopping" %(url_file))
+    sys.exit()
+
+extension_file = urls_name+".csv"
+if(resume):
+    print("File %s exists, Resuming download" %(urls_name))
+    with open(extension_file, 'r') as f:
+        reader = csv.reader(f)
+        already_done = str(list(reader))
+else:
+    open(extension_file, 'w').close()
+    print("File %s exists, starting to download" %(urls_name))
+
+#Create download folder
+download_folder = os.path.join(crx_folder, urls_name)
+create_directory(download_folder)
+
+#Get list of extensions
+extension_list = open(url_file, 'r', encoding='utf-8') #,encoding="utf8"
+extension_urls = extension_list.readlines()
+extension_urls = [x.strip() for x in extension_urls]
+
+no_extensions = len(extension_urls)
+
+print(no_extensions)
+
+with tqdm(total=no_extensions) as pbar:
+    for url in extension_urls:
+        list_file = open(extension_file, 'a', encoding='utf-8') #
+        if resume and url in already_done:
+            if verbose:
+                tqdm.write("skipping %s"%(url))
+        else:
+            if verbose:
+                tqdm.write("Downloading %s "%(url))
+        #get html
+        page = urllib.request.urlopen(url)
+
+        # parse the html using beautiful soup and store in variable `soup`
+        soup = BeautifulSoup(page, 'html.parser')
+
+        name_box = soup.find('h1', attrs={'class': 'Pa2dE'})
+        extension_name = name_box.text.strip()
+
+        producer_box = soup.find('div', attrs={'class':'C2WXF'})
+        #print(list(producer_box.stripped_strings))
+        producer_name, producer_company, producer_address = producer_box.stripped_strings
+        producer_address = producer_address.replace("\n", '')
+
+
+        category_box = soup.find('a', attrs={'class':'gqpEIe bgp7Ye'})
+        extension_category = category_box.text.strip()
+
+        population_box = soup.find(string=lambda text: text and re.search(r"\d+,?\d+ users", text))
+        extension_population = population_box.text.strip()
+        extension_population = extension_population.replace(' users', '')
+
+        ratings_box = soup.find('span', attrs={'class':'Vq0ZA'})
+        extension_ratings = ratings_box.text.strip()
+
+        no_ratings_box = soup.find('p', attrs={'class': 'xJEoWe'})
+        no_ratings = no_ratings_box.text.strip()
+
+        str2=url.split('/')
+        n = len(str2)
+        url = "https://clients2.google.com/service/update2/crx?response=redirect&os=linux&arch=x64&os_arch=x86_64&nacl_arch=x86-64&prod=chromium&prodchannel=unknown&prodversion=91.0.4442.4&lang=en-US&acceptformat=crx2,crx3&x=id%3D"+ str2[n-1] + "%26installsource%3Dondemand%26uc"
+        #Try to download file
+        for attempt in range(5):
+            try:
+                extension = '.crx'
+                file = re.sub('[<>:"/\|?*,.]',' ',extension_name)
+                #Checks if the file already exists, if it does creates a versions with number at the end
+                if os.path.isfile(os.path.join(download_folder, file+".crx")):
+                    i = 1
+                    while os.path.isfile(os.path.join(download_folder,file+'_'+str(i)+".crx")):
+                        i += 1
+
+                    urllib.request.urlretrieve(url, os.path.join(download_folder,file+'_'+str(i)+".crx"))
+                    file_name  = os.path.join(file+'_'+str(i)+".crx")
+                else:
+                    urllib.request.urlretrieve(url, os.path.join(download_folder,file+".crx"))
+                    file_name = file+".crx"
+                break
+            except Exception as e:
+                if attempt == 4:  # This is the last attempt
+                    errors_file = open('Error URL.txt', 'a', encoding='utf-8')
+                    errors_file.write(ext+' -Downloading extension\n')
+                    errors_file.write(str(e))
+                    tqdm.write(extension_name+" couldn't be downloaded")
+                    errors_file.close()
+                    file_name = "error"
+                    #driver.get('http://chrome-extension-downloader.com/')
+
+        #Write extension details to file
+        list_file.write(re.sub('[<>:"/\|?*,]',' ',extension_name) + ',' + url + ',' + re.sub('[<>:"/\|?*,]',' ',producer_name) + ',' + re.sub('[<>:"/\|?*,]',' ',producer_company) + ',' + re.sub('[<>:"/\|?*,]',' ',producer_address) + ',' + extension_category + ',' + re.sub(",",'',extension_population) + ',' + extension_ratings + ',' + re.sub("ratings",'',no_ratings) + ',' + file_name+ '\n')
+        list_file.close()
+        #time.sleep(2)
+
+    #Update progess bard
+    pbar.update(1)
