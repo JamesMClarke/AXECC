362e36d283c0dd618baea17d3106138e97f81fb3
James Clarke <jamesmorganclarke@hotmail.co.uk>
2024-06-18 22:48:35 +0100

    Added being able to preprocess files anywhere

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,29 @@ from common import *
 def create_directory(directory_path):
     Path(directory_path).mkdir(parents=True, exist_ok=True)
 
+def sanitize_filename(filename):
+    # Replace invalid characters with an underscore or other valid character
+    invalid_chars = '<>:"/\\|?*'
+    sanitized_filename = ''.join('_' if c in invalid_chars else c for c in filename)
+    return sanitized_filename
+
+def extract_zip_safely(zip_path, extract_to):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        for member in zip_ref.infolist():
+            # Sanitize the filename
+            original_filename = member.filename
+            sanitized_filename = sanitize_filename(original_filename)
+
+            # Prepare the target path
+            target_path = os.path.join(extract_to, sanitized_filename)
+
+            # Ensure the directory exists
+            os.makedirs(os.path.dirname(target_path), exist_ok=True)
+
+            # Extract the file
+            with zip_ref.open(member) as source, open(target_path, "wb") as target:
+                target.write(source.read())
+
 current_dir = os.getcwd()
 
 #TODO: Add the ablity to include multiple folders
@@ -22,15 +45,19 @@ verbose = args.verbose
 sql_name = os.path.basename(sql_file).split(".")[0]
 
 #Check if csv file exists
-sql_file = os.path.join(current_dir,sql_file)
+#sql_file = os.path.join(current_dir,sql_file)
+#print(sql_file)
+category_folder = os.path.dirname(sql_file)
+#print(all_files)
+#quit()
 if(os.path.isfile(sql_file)):
     print("File %s exists, starting preprocessing" %(sql_name))
 else:
     print("File %s doesn't exists, stopping" %(sql_file))
     sys.exit()
 
-all_files = os.path.join(current_dir,"extensions")
-category_folder = os.path.join(all_files, sql_name)
+#all_files = os.path.join(current_dir,"extensions")
+#category_folder = os.path.join(all_files, sql_name)
 
 #Set dir for crx files
 crx_dir = os.path.join(category_folder,"crx_files")
@@ -56,19 +83,21 @@ with tqdm(total=no_rows) as pbar:
                 if verbose:
                     tqdm.write("Folder already exists")
                     tqdm.write(file)
-                    tqdm.write(row[1])
+                    #tqdm.write(row[1])
 
             #Try to unzip file
             try:
-                with zipfile.ZipFile(os.path.join(crx_dir,file), 'r') as zip_ref:
-                    unzipped += 1
-                    zip_ref.extractall(os.path.join(unzip_dir,file.split('.')[0]))
+                #with zipfile.ZipFile(os.path.join(crx_dir,file), 'r') as zip_ref:
+                #    unzipped += 1
+                #zip_ref.extractall(os.path.join(unzip_dir,file.split('.')[0]))
+                extract_zip_safely(os.path.join(crx_dir,file),os.path.join(unzip_dir,file.split('.')[0]))
             except Exception as e :
                 if verbose:
                     tqdm.write("Error unzipping %s"%file)
+                    tqdm.write(str(e))
                 errors += 1
                 errors_file = open('Error Unzip.csv', 'a', encoding='utf-8')
-                errors_file.write(row[0]+row[1]+file+str(e)+'\n')
+                errors_file.write(file+','+str(e)+','+'\n')
                 errors_file.close()
         else:
             tqdm.write("File %s doesn't exist"%file)
